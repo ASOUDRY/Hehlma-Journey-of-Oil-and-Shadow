95a866f51aa52c4d7969c266c52ba1f2cd83d2fc
Alexander Soudry <soudryalex@gmail.com>
2023-01-19 18:46:09 -0500

    initial  routing component and implentation of it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MainGameComponent } from './main-game/main-game.component';
+import { CharacterCreationComponent } from './character-creation/character-creation.component';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hehl', component:MainGameComponent},
+  { path: 'character', component:CharacterCreationComponent},
+  { path: 'login-register', component:LoginRegistrationComponent},
+  { path: '**', component:LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
