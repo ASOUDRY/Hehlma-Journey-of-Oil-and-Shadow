2d0562194bda3aefb0507bf9f976208b20601dd5
Alexander Soudry <soudryalex@gmail.com>
2023-01-19 18:36:40 -0500

    initial commit for the app's routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MainGameComponent } from './main-game/main-game.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { CharacterCreationComponent } from './character-creation/character-creation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    MainGameComponent,
+    CharacterCreationComponent,
+    LoginRegistrationComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
