4266c47dbb2a5ad0c1350b375ffb7be8f55e12fb
Alexander Soudry <soudryalex@gmail.com>
2023-02-10 14:29:16 -0500

    added additional services to access api endpoints.

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from  '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { FetchPlayerPayload } from '../interfaces/fetch-player-payload';
+import { RegistrationPayload } from '../interfaces/registration-payload';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class ServiceService {
   private url = 'http://localhost:5001/api/Monster';
 
  // private url = 'https://testapp-as.azurewebsites.net/WeatherForecast';
+//  private locationurl = 'http://localhost:5001/Location/Royal%20City
 
   constructor(private http: HttpClient) { }
 
@@ -19,7 +21,45 @@ export class ServiceService {
     return this.http.get(this.url);
   }
 
-  getLocation() {
-    return this.http.get('http://localhost:5001/api/Monster')
+  getLocation(key: string) {
+    // `Hello, ${name}`
+    return this.http.get(`http://localhost:5001/Location/${key}`)
+  }
+
+  getAdventure(key: string) {
+    return this.http.get(`http://localhost:5001/api/Adventure/${key}`)
+    //return this.http.get(`http://localhost:5001/Location/${key}`)
+  }
+
+  getPlayer(key: FetchPlayerPayload) {
+    return this.http.post(`http://localhost:5001/Player`, key)
+    //return this.http.get(`http://localhost:5001/Location/${key}`)
+  }
+
+
+  registration(payload: RegistrationPayload) {
+    return this.http.post(`http://localhost:5001/api/Registration`, payload)
+  }
+
+  login(payload: any) {
+    return this.http.post(`http://localhost:5001/Login`, payload)
+  }
+
+
+  private data: any;
+
+  setData(data: any) {
+    this.data = data;
+  }
+
+  setLogin(username: string, id: string) {
+    this.data = {
+      "username": username,
+       "id": id
+    }
+  }
+
+  getData() {
+    return this.data;
   }
 }
