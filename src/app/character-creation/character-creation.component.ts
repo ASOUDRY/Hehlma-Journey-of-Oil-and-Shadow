c85a8248a4b321c0197cff0c6d53ba80033f00a9
Alexander Soudry <soudryalex@gmail.com>
2023-02-10 14:28:10 -0500

    added functionality to the component, allowing for the cards to pass values to the registration component.

--- src/app/character-creation/character-creation.component.ts
+++ src/app/character-creation/character-creation.component.ts
@@ -8,11 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./character-creation.component.css']
 })
 export class CharacterCreationComponent {
- // value = 'Clear me';
-  value3 = "";
+  hasNotDecided: boolean = true;
+  login: boolean = true;
 
   value="";
   clearValue() {
     this.value="";
+  }
+
+  access(value: string) {
+    if (value === 'login') {
+      this.login = true;
+    }
+    else {
+      this.login = false;
+    }
+    this.hasNotDecided = !this.hasNotDecided;
   }
 }
