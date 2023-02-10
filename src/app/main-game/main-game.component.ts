ed4a1e4b2278892976818fb62bfd46829b20d270
Alexander Soudry <soudryalex@gmail.com>
2023-02-10 13:18:54 -0500

    Updated component to pull data from my web api and store it in usual variables.

--- src/app/main-game/main-game.component.ts
+++ src/app/main-game/main-game.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { ServiceService } from '../services/service.service';
 import { Messages } from '../interfaces/messages';
+import { Router } from '@angular/router';
+import { getLocaleDateFormat } from '@angular/common';
+import { firstValueFrom } from 'rxjs';
+import { FetchPlayerPayload } from '../interfaces/fetch-player-payload';
+import { CharacterSheet } from '../interfaces/character-sheet';
 
 @Component({
   selector: 'app-main-game',
@@ -9,18 +14,70 @@ import { Messages } from '../interfaces/messages';
 })
 export class MainGameComponent {
   posts : any;
-  constructor(private httpService: ServiceService) { }
-  
+  name: any;
+  tests: any;
+  action: string = "";
+  notShopping: boolean = true;
+  storedObject: any;
+  receive: any;
+  player: CharacterSheet = {
+    name: "",
+    attack: 0,
+    defense: 0,
+    health: 0,
+    skill: "",
+    class: "",
+    bonusAttack: 0,
+    bonusDefense: 0,
+    bonusHealth: 0,
+    inventory: []
+  }
+
+  constructor(private httpService: ServiceService, private router: Router) { }
   ngOnInit() {
-    // this.httpService.getPosts().subscribe(
-    // (response) => { this.posts = response; },
-    // (error) => { console.log(error); });
-    this.httpService.getLocation().subscribe(
-      (response) => { this.posts = response; 
-        console.log(response);
-      },
-      (error) => { console.log(error); });
-      
+
+    const storedObjectString = sessionStorage.getItem("LoginCredentials");
+    console.log(storedObjectString)
+    if (storedObjectString) {
+    this.storedObject = JSON.parse(storedObjectString);
+    }
+    console.log(this.storedObject);
+    this.getDataFromApi()
+  }
+
+  async getDataFromApi() {
+    try {
+      const payload: FetchPlayerPayload = this.storedObject;
+
+      const response = await firstValueFrom(this.httpService.getPlayer(this.storedObject))
+      console.log(response);
+      this.receive = response;
+      this.player.name = this.receive.characterName;
+      this.player.attack = this.receive.attack;
+      this.player.defense = this.receive.defense;
+      this.player.health = this.receive.hitpoints;
+      this.player.class = this.receive.cClass;
+        for (let i = 0; i  < this.receive.inventory.length; i++) {
+          this.player.inventory[i] = {
+            name: this.receive.inventory[i].name,
+            description: this.receive.inventory[i].description,
+            magical: this.receive.inventory[i].magical,
+            quantity: this.receive.inventory[i].quantity
+          }
+        }
+      this.player.skill = this.receive.skill;
+      console.log(this.player)
+    
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const response = await firstValueFrom(this.httpService.getLocation('Kap'))
+      this.posts = response; 
+      console.log(this.posts);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 m1 : Messages = {
@@ -31,21 +88,45 @@ m1 : Messages = {
 }
   message = "";
   undecided = true;
+  shopping : boolean = false;
  
   onEditClick(value: string) {
+    console.log(value);
     this.undecided = !this.undecided;
-    if (value = this.m1.first) {
-this.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    if (value == this.m1.first) {
+    this.message = `You find the ${this.posts[0].npcType} ${this.posts[0].npcName}.${this.posts[0].npcInfo}`;
+    this.action = "What would you like to do?";
+    console.log(value);
     }
-    else if (value = this.m1.second) {
+    else if (value == this.m1.second) {
       this.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     }
      else {
-      this.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+      this.message = `You wander over to the ${this.posts[0].aName} ${this.posts[0].npcName}.`;
+      this.action = `${this.posts[0].aInfo}`;
     }
   }
 
 onLeave() {
   this.undecided = !this.undecided;
 }
+
+onRouting() {
+  this.router.navigateByUrl(`/adventure/${this.posts[0].next}`);
+}
+
+onAction(value: string) {
+  this.notShopping = !this.notShopping;
+  if (value === 'Shopping') {
+    this.shopping = true;
+  }
+  else {
+    this.shopping = false;
+  }
+}
+
+reverse(value: boolean) {
+this.notShopping = value
+}
+
 }
