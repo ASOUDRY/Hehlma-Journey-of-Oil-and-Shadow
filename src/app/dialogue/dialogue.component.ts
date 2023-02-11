e01510d69cb6c34ab3f695dde8cadefdc8cbb4aa
Alexander Soudry <soudryalex@gmail.com>
2023-02-10 21:17:07 -0500

    removed code for the moment.

--- src/app/dialogue/dialogue.component.ts
+++ src/app/dialogue/dialogue.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-dialogue',
@@ -6,12 +6,5 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./dialogue.component.css']
 })
 export class DialogueComponent {
-leave: string = "Nothing";
-dialogue : string = "Howdy do travelor my name is Lexie. You looking alright to me. What can I do you for?";
 
-@Output() backToOption = new EventEmitter<boolean>();
-
-onLeave() {
-  this.backToOption.emit(true)
-}
 }
