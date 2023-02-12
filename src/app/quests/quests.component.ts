a3155fea636d863ca7584d798cf229693ebd9b75
Alexander Soudry <soudryalex@gmail.com>
2023-02-11 19:59:04 -0500

    started programming the quest component. It retreives a small amount of api quest data from my api.

--- src/app/quests/quests.component.ts
+++ src/app/quests/quests.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { ServiceService } from '../services/service.service';
 import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-quests',
@@ -10,29 +12,48 @@ export class QuestsComponent {
   posts: any = undefined;
   questId: string = "";
   origin: string = "";
-  giver: string = ""
+  response: string = ""
+  giver: string = 'Dachs Kraus'
+  deliberating: boolean = true;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private httpService: ServiceService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.getApiData()
     this.questId = this.route.snapshot.params['lName'];
     this.origin = this.route.snapshot.params['origin'];
     if (this.origin == "The Drunken Squid") {
-      this.giver = "Tavern Goer"
+    this.giver = "Dachs Kraus"
     }
+    this.getApiData();
   }
 
-leave: string = "Nothing";
-dialogue : string = "Howdy do travelor my name is Lexie. You looking alright to me. What can I do you for?";
+  leave: string = "Nothing";
+  dialogue : string = "Howdy do travelor my name is Lexie. You looking alright to me. What can I do you for?";
 
-@Output() backToOption = new EventEmitter<boolean>();
+  @Output() backToOption = new EventEmitter<boolean>();
 
-onLeave() {
-  this.backToOption.emit(true)
-}
+  onLeave() {
+    this.backToOption.emit(true)
+  }
 
-getApiData() {
+  onAccept(value: boolean) {
+    if (value) {
+      this.response = "Good Luck"
+    }
+    else {
+      this.response = "Sod off Then"
+    }
+    this.deliberating = !this.deliberating
+  }
 
-}
+  async getApiData() {
+    try {
+      const response = await firstValueFrom(this.httpService.getQuest(this.giver, 'Kap'))
+      this.posts = response
+      console.log(this.posts)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
