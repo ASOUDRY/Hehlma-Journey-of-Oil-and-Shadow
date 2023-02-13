b41d9a434c4a6cbd0021c56248f08301117260f9
Alexander Soudry <soudryalex@gmail.com>
2023-02-12 22:01:57 -0500

    minor updates.

--- src/app/quests/quests.component.ts
+++ src/app/quests/quests.component.ts
@@ -1,7 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { ServiceService } from '../services/service.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
+import { CharacterSheet } from '../interfaces/character-sheet';
 
 @Component({
   selector: 'app-quests',
@@ -10,13 +11,26 @@ import { firstValueFrom } from 'rxjs';
 })
 export class QuestsComponent {
   posts: any = undefined;
+  player: CharacterSheet = {
+    name: "",
+    attack: 0,
+    defense: 0,
+    health: 0,
+    skill: "",
+    class: "",
+    bonusAttack: 0,
+    bonusDefense: 0,
+    bonusHealth: 0,
+    inventory: [],
+    Quest: ""
+  };
   questId: string = "";
   origin: string = "";
   response: string = ""
   giver: string = 'Dachs Kraus'
   deliberating: boolean = true;
 
-  constructor(private httpService: ServiceService, private route: ActivatedRoute) { }
+  constructor(private httpService: ServiceService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.getApiData()
@@ -25,7 +39,8 @@ export class QuestsComponent {
     if (this.origin == "The Drunken Squid") {
     this.giver = "Dachs Kraus"
     }
-    this.getApiData();
+    // this.getApiData();
+    this.getStorageData()
   }
 
   leave: string = "Nothing";
@@ -37,14 +52,27 @@ export class QuestsComponent {
     this.backToOption.emit(true)
   }
 
+  getStorageData() {
+  let playerData: any = sessionStorage.getItem('player');
+  this.player = JSON.parse(playerData);
+  console.log(this.player);
+  }
+
   onAccept(value: boolean) {
     if (value) {
       this.response = "Good Luck"
+      console.log(this.posts.identifer);
+      this.player.Quest = this.posts.identifer
+      this.httpService.updatePlayer(this.posts.identifer)
     }
     else {
       this.response = "Sod off Then"
     }
     this.deliberating = !this.deliberating
+  }
+
+  onRouting(value: string) {
+    this.router.navigateByUrl(`/adventure/${value}`);
   }
 
   async getApiData() {
@@ -55,5 +83,9 @@ export class QuestsComponent {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  ngOnDestroy() {
+    sessionStorage.setItem('player', JSON.stringify(this.player));
   }
 }
