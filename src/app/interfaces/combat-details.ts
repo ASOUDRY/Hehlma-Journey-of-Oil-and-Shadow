5ae75dd02445650b9788f3d70206c6d414270311
Alexander Soudry <soudryalex@gmail.com>
2023-04-03 11:18:48 -0400

    New interface to retreive data from a api endpoint.

---
+++ src/app/interfaces/combat-details.ts
@@ -0,0 +1,8 @@
+export interface CombatDetails {
+    character_attack: string,
+    monster_attack1: string,
+    monster_attack2: string,
+    dodge_action: string,
+    dodged_attacks: string,
+    lose_choice: string
+}
