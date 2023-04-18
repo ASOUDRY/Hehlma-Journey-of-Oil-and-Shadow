a5f62c5cf9278882fab590cef96ce50b860986c2
Alexander Soudry <soudryalex@gmail.com>
2023-04-18 17:41:39 -0400

    minor tweaks.

--- src/app/adventuring-components/stealth/stealth.component.ts
+++ src/app/adventuring-components/stealth/stealth.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AdventurePackage } from 'src/app/interfaces/adventure-package';
 
 @Component({
   selector: 'app-stealth',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class StealthComponent {
 
+
+  @Input() monsterData: AdventurePackage = {
+    questButton1: "",
+    questButton2: "",
+    adventureOption1: '',
+    adventureOption2: '',
+    attack: 0,
+    bootyReward: '',
+    defense: 0,
+    dodgeAttack: '',
+    enviormentDescription: '',
+    exploration1: '',
+    exploration2: '',
+    exploration3: '',
+    fightEnviormentDescription1: '',
+    fightEnviormentDescription2: '',
+    hitpoints: 0,
+    locationDescription: '',
+    locationName: '',
+    monsterAttack1: '',
+    monsterAttack2: '',
+    monsterFlee: '',
+    monsterName: '',
+    nextLocation: '',
+    questName: '',
+    questdialogue1: '',
+    questdialogue2: '',
+    questdialogue3: '',
+    stealth1: '',
+    stealth2: '',
+    uniqueAttackDescription: '',
+  };
+
+  primary: string = "You attempt to sneak past the Owalarsterin, carefully creaping along all the debree. Eventually the creature submerges back into the oily water and you are home free."
+
+  secondary: string = "Yosh"
 }
