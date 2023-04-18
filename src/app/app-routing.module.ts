3c2c6c49ac59407f117a55b7a7a827ed6af1b4d7
Alexander Soudry <soudryalex@gmail.com>
2023-04-18 17:47:20 -0400

    tweaked the routing to work on component and test it easiar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MainGameComponent } from './main-game/main-game.component';
 import { CharacterCreationComponent } from './loginAndRegistration/character-creation/character-creation.component';
 import { QuestsComponent } from './social-components/quests/quests.component';
+import { TravelComponent } from './travel/travel.component';
 
 import { AdventureComponent } from './adventuring-components/adventure/adventure.component';
 
 const routes: Routes = [
   { path: 'hehl', component:MainGameComponent},
   { path: 'character', component:CharacterCreationComponent},
-  {path: ':locations/:origin/quests', component:QuestsComponent},
-  {path: 'adventure/:name', component:AdventureComponent},
-  { path: '**', component:LandingPageComponent}
+  { path: ':locations/:origin/quests', component:QuestsComponent},
+  // { path: 'adventure/:name', component:AdventureComponent},
+  { path: 'proxy/:name', component:AdventureComponent},
+  { path: 'traveling', component:TravelComponent},
+  { path: '**', component:LandingPageComponent},
 ];
 
 @NgModule({
