38653b42a5b3e8116e0cbf31338040d6cd7bb98f
Alexander Soudry <soudryalex@gmail.com>
2023-04-18 17:46:35 -0400

    added new endpoints.

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -15,40 +15,31 @@ export class ServiceService {
 
   private url2 =  'https://hehl-api.azurewebsites.net/';
 
- // private url = 'https://testapp-as.azurewebsites.net/WeatherForecast';
-//  private locationurl = 'http://localhost:5001/Location/Royal%20City
-
-// private quest1 = { quest: "A kid was lost at the stirring sea. Can you go fetch him for me.", rescue, lowgrade }
-
   constructor(private http: HttpClient) { }
 
-  getPosts() {
-    return this.http.get(this.url);
-  }
+  // getPosts() {
+  //   return this.http.get(this.url);
+  // }
 
   getQuest(key1: string, key2: string) {
     return this.http.get(`http://localhost:5001/api/Quest/${key1}/${key2}`)
   }
 
-  getLocation(key: string) {
-    // `Hello, ${name}`
-  
-  // return this.http.get(`${this.url2}${key}`)
-  return this.http.get(`https://hehl-api.azurewebsites.net/Location/${key}`);
-  //  return this.http.get(`http://localhost:5001/Location/${key}`)
+  getLocation(key: string) { 
+    return this.http.get(`http://localhost:5001/Location/${key}`)
   }
 
   getAdventure(key: string) {
     return this.http.get(`http://localhost:5001/api/Adventure/${key}`)
-    //return this.http.get(`http://localhost:5001/Location/${key}`)
+  }
+
+  getQuestEncounter(key: string) {
+    return this.http.get(`http://localhost:5001/Encounter/${key}`)
   }
 
   getPlayer(key: FetchPlayerPayload) {
     return this.http.post(`${this.url2}/Player`, key)
-   // return this.http.post(`http://localhost:5001/Player`, key)
-    //return this.http.get(`http://localhost:5001/Location/${key}`)
   }
-
 
   registration(payload: RegistrationPayload) {
     return this.http.post(`http://localhost:5001/api/Registration`, payload)
@@ -56,7 +47,6 @@ export class ServiceService {
 
   login(payload: any) {
     return this.http.post(`${this.url2}/Login`, payload)
-    return this.http.post(`http://localhost:5001/Login`, payload)
   }
 
   private playerData: any;
