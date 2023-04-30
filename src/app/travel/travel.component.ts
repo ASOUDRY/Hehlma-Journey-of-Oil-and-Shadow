3146db045652981737dd8aae69b91753ca9a7748
Alexander Soudry <soudryalex@gmail.com>
2023-04-30 18:50:42 -0400

    started working on the component. It receives different url parameters based on which button is clicked to route to the component.

--- src/app/travel/travel.component.ts
+++ src/app/travel/travel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-travel',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class TravelComponent {
 
+  constructor (private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const key = params.get('direction');
+      const key2 = params.get('name');
+      this.travel =   String (key);
+      this.destination = String (key2);
+    })
+  }
+
+  travel: string = "";
+  destination: string = "";
+
+  data: any = {
+    para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    para2:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }
+
+  navigate() {
+    this.router.navigateByUrl(`settlement/${this.destination}`);
+  }
 }
