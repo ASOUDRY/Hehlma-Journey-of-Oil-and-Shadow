e0ea215a5bd512f53bc8ee8e5f7103b7fa9a892f
Alexander Soudry <soudryalex@gmail.com>
2023-04-30 19:19:13 -0400

    functionally rewrote the component, having it receive multiple small objects from services and sending these smaller objects to the various linked components for better development ease.

--- src/app/adventuring-components/adventure/adventure.component.ts
+++ src/app/adventuring-components/adventure/adventure.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AdventurePackage } from '../../interfaces/adventure-package';
 import { CharacterSheet } from '../../interfaces/character-sheet';
 import { ServiceService } from '../../services/service.service';
@@ -16,34 +16,65 @@ import { StealthData } from 'src/app/interfaces/StealthData';
 })
 export class AdventureComponent {
 
-  fight: Fight = {
-    monsterName: "",
-    hitpoints: 0 ,
-    attack: 0,
-    defense: 0,
-    monsterAttack1: "",
-    monsterAttack2: "",
-    dodgeAttack: "",
-    monsterFlee: "",
-    description1: "",
-    description2: "",
-    description3: "",
-    uniqueAttackDescription: ""
+  constructor(private httpService: ServiceService, private route: ActivatedRoute, private router: Router) {}
+
+  navigateToNewLocation(value: number) {
+    console.log("reached the Apex")
+    console.log(value);
+
+    if (value == 1) {
+      this.option1 = false;
+    }
+    else if (value == 0) {
+      this.router.navigateByUrl(`/retreatingTo/Kap`);
+    }
   }
 
-  constructor(private httpService: ServiceService, private route: ActivatedRoute) {}
+    combat : any = {
+    hiddenTactic: true,
+    attack_option : "Boldly attack with your weapon",
+    attackDescription: "You swing your sword with great force slicing the beast. Cutting its tentacle. Oil spills from its body just like that, plastering you all black with the goop.",
+    dodge_action: "You dodge behind some flotsam or other material",
+    dodge_option : "Duck behind some flotsam to wait and hide and",
+    rewardType: "combat"
+    }
 
-  adventureOption: string = "";
-  counter: number = 0
-  name: string = "";
-  hasQuest: boolean = true;
+
   anyData: any
+  counter: number = 0
+
+  hasQuest: boolean = true;
   decision: boolean = true;
   option1: boolean = true;
 
-fightOption0 : string = "Wait and see what to do"
-fightOption1 : string = "Attempt to sneak past the beast"
-fightOption2 : string = "Fight"
+  adventureOption: string = "";
+  fightOption0 : string = "Wait and see what to do"
+  fightOption1 : string = "Attempt to sneak past the beast"
+  fightOption2 : string = "Fight"
+  name: string = "";
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const key = params.get('name');
+      this.name =   String (key);
+    })
+    this.getDataFromApi();
+    this.getStorageData()
+  }
+
+
+  bootyObject: any = {
+  string1: "You spot something on the ground",
+  string2: "Pick it Up",
+  string3: "Leave it",
+  string4: "You find a piece of Olwarstein oil on your hand. Its luke warm and quite nasty. But you should be able to sell it for a pretty bit of coinage. You Pocket it",
+  item: {
+    name: 'Olwarstein Horn.',
+    description: 'A disgustingly fleshy piece of chitinous mass. Useful for crafting magical weaponey.',
+    magical: true,
+    quantity: 100
+},
+};
 
   adventureData: AdventurePackage = {
     locationName: "",
@@ -78,6 +109,22 @@ fightOption2 : string = "Fight"
     description3: "",
     uniqueAttackDescription: ""
   }
+
+  questFightData: Fight ={
+    monsterName: "",
+    hitpoints: 0 ,
+    attack: 0,
+    defense: 0,
+    monsterAttack1: "",
+    monsterAttack2: "",
+    dodgeAttack: "",
+    monsterFlee: "",
+    description1: "",
+    description2: "",
+    description3: "",
+    uniqueAttackDescription: ""
+  }
+
   stealthData: StealthData = {
     stealth1: "",
     stealth2: ""
@@ -95,34 +142,19 @@ fightOption2 : string = "Fight"
     quest: ""
   };
 
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      const key = params.get('name');
-      this.name =   String (key);
-      console.log(this.name);
-    })
-    this.getDataFromApi();
-    this.getStorageData()
-  }
-
   async getDataFromApi() {
-    this.anyData = await firstValueFrom(this.httpService.getAdventure(this.name));
-    console.log(this.anyData);
-    
+    this.anyData = await firstValueFrom(this.httpService.getAdventure(this.name));    
     this.anyData = this.httpService.packageParser(this.anyData, 0);
 
-    this.adventureData = this.anyData[0];
-    
-    this.fightData = this.anyData[1];
-
-    this.questData = this.anyData[2];
-
-    this.stealthData = this.anyData[4];
     console.log(this.anyData);
-
+    Object.assign(this.adventureData, this.anyData[0]);
+    Object.assign(this.fightData, this.anyData[1]);
+    Object.assign(this.questData, this.anyData[2]);
+    Object.assign(this.questFightData, this.anyData[3]);
+    Object.assign(this.stealthData, this.anyData[4]);
+    console.log(this.fightData);
+    console.log(this.questFightData);
   }
-
-
 
   getStorageData() {
     let playerData: any = sessionStorage.getItem('player');
@@ -157,10 +189,10 @@ fightOption2 : string = "Fight"
       this.adventureOption = 'combat';
     }
   }
-    escape(value: string) {
+  escape(value: string) {
       if (value == "prior_decision") {
         this.decision = true;
       }
       this.adventureOption = '';
-    }
+  }
 }
